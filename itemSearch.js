25f0d663ed54f5d852651a2062085cdd4552ae74
MirMoneyJ <132304703+MirMoneyJ@users.noreply.github.com>
2023-05-03 18:34:07 -0400

    Update itemSearch.js

--- itemSearch.js
+++ itemSearch.js
@@ -1,3 +1,9 @@
+/*
+file name: itemSearch.js
+purpose: creates Schema for items all of type String
+authors: Lawrence, Ahmir, Krishna, Chic, Trent, Mya
+version: 4/30/23
+*/
 const mongoose = require('mongoose');
 
 // Define the schema for the craftable item
